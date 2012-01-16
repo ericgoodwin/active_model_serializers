ed6e6f32a2cc8c728e07f836079e44db6ec08f30
Eric Goodwin <eric@ericgoodwin.com>
2012-01-16 12:14:01 -0800

    Serialization scope should be on the class

--- lib/action_controller/serialization.rb
+++ lib/action_controller/serialization.rb
@@ -33,7 +33,7 @@ module ActionController
     end
 
     def serialization_scope
-      send(_serialization_scope)
+      send(self.class._serialization_scope)
     end
 
     def default_serializer_options
